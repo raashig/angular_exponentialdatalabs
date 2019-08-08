19ef1cec00560caa10ca3aad423fdf61ef08a109
raashig <golchharaashi@gmail.com>
2019-08-08 21:31:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nruvdd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nruvdd)
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "tables",
+  templateUrl: "./table.component.html",
+  styleUrls: ["./table.component.css"]
+})
+export class TableComponent {}
